dce345dd5f65e57fab5389cc221b6c21e04ef73f
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-12-20 12:21:38 +0300

    [feat] NoneAdService.cs

---
+++ Runtime/Services/NoneAdService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using AdsIntegration.Runtime.Base;
+using JetBrains.Annotations;
+using R3;
+
+namespace AdsIntegration.Runtime.Services
+{
+    [PublicAPI]
+    internal sealed class NoneAdService : IAdService
+    {
+        public Observable<bool> OnRewardedAdAvailabilityChanged => _rewardedAdAvailabilityChanged;
+        private readonly Subject<bool> _rewardedAdAvailabilityChanged = new();
+
+        public void Init() { }
+        public bool ShowRewardedAd(Enum placement, Action onRewarded) => false;
+        public bool IsRewardedAdAvailable() => false;
+        public bool TryShowInterstitial() => false;
+    }
+}
